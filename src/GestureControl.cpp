2912017a0c5b3618af3d8d2f2547f1475e15c2a5
Jordan Horwich <jordan@voidingwarranties.com>
2014-04-12 22:27:19 -0500

    Clean up code

--- src/GestureControl.cpp
+++ src/GestureControl.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <utility>
+#include <algorithm>
 
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-const cv::Scalar orange_MIN(60, 140, 60);
+/* TO DO:
+ * - background subtraction so that only moving objects are considered for tracking
+ */
+
+#define MIN_CIRCULARITY 0.8
+#define MIN_AREA 500
+
+const cv::Scalar orange_MIN(60, 135, 60);
 const cv::Scalar orange_MAX(230, 190, 100);
 
 cv::Mat global_image;
@@ -56,6 +65,37 @@ cv::Mat preProcess(const cv::Mat& image)
         return pp_image;
 }
 
+bool contour_comp(const std::pair<std::vector<cv::Point>, float>& a, const std::pair<std::vector<cv::Point>, float>& b)
+{
+    return (a.second > b.second);
+}
+
+std::vector<std::pair<std::vector<cv::Point>, float> > getMostCircularObjects(const cv::Mat& image)
+{
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<std::pair<std::vector<cv::Point>, float> > contour_circularities;
+    cv::findContours(image, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+    for (size_t i = 0; i < contours.size(); ++i) {
+        double perimeter = cv::arcLength(contours[i], true);
+        double area = cv::contourArea(contours[i]);
+        double circularity = 4 * 3.14159 * area / (perimeter * perimeter);
+        contour_circularities.push_back(std::pair<std::vector<cv::Point>, float>(contours[i], circularity));
+        //cv::drawContours(cam_image, contours, max_circularity_i, cv::Scalar(255,0,0), 2);
+    }
+
+    //std::sort(contour_circularities.begin(), contour_circularities.end(), [](const std::pair<std::vector<cv::Point>, float>& a, const std::pair<std::vector<cv::Point>, float>& b) { return (a.second > b.second); });
+    std::sort(contour_circularities.begin(), contour_circularities.end(), contour_comp);
+
+/*
+    if (max_circularity > 0.77) {
+        cv::drawContours(cam_image, contours, max_circularity_i, cv::Scalar(0,255,0), 2);
+        cv::drawContours(cam_image, contours, max_circularity_i, cv::Scalar(0,0,255), -1);
+    }
+*/
+
+    return contour_circularities;
+}
+
 int main(int argc, char** argv)
 {
     cv::namedWindow("cam");
@@ -71,25 +111,19 @@ int main(int argc, char** argv)
         cv::namedWindow("foobar");
         cv::imshow("foobar", pp_image);
 
-        std::vector<std::vector<cv::Point> > contours;
-        cv::findContours(pp_image, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
-        double max_circularity = 0;
-        size_t max_circularity_i = 0;
-        for (size_t i = 0; i < contours.size(); ++i) {
-            double perimeter = cv::arcLength(contours[i], true);
-            double area = cv::contourArea(contours[i]);
-            double circularity = 4 * 3.14159 * area / (perimeter * perimeter);
-            if (circularity > max_circularity && area > 500) {
-                max_circularity = circularity;
-                max_circularity_i = i;
+        std::vector<std::pair<std::vector<cv::Point>, float> > contour_circularity = getMostCircularObjects(pp_image);
+        size_t num_objects = 1;
+        std::vector<std::vector<cv::Point> > most_circular;
+        for (size_t i = 0; i < contour_circularity.size(); ++i) {
+            if (contour_circularity[i].second > MIN_CIRCULARITY && cv::contourArea(contour_circularity[i].first) > MIN_AREA) {
+                most_circular.push_back(contour_circularity[i].first);
+                if (most_circular.size() >= num_objects) {
+                    break;
+                }
             }
-            cv::drawContours(cam_image, contours, max_circularity_i, cv::Scalar(255,0,0), 2);
         }
-
-        if (max_circularity > 0.77) {
-            cv::drawContours(cam_image, contours, max_circularity_i, cv::Scalar(0,255,0), 2);
-            cv::drawContours(cam_image, contours, max_circularity_i, cv::Scalar(0,0,255), -1);
-        }
+        cv::drawContours(cam_image, most_circular, -1, cv::Scalar(0,255,0), 2);
+        cv::drawContours(cam_image, most_circular, -1, cv::Scalar(0,0,255), -1);
 
         cv::imshow("cam", cam_image);
 
